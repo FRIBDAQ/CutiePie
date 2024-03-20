c05c8e4e309513d5b3f0c439cd1694d8f8df1221
Ron Fox <fox@frib.msu.edu>
2024-03-20 08:12:25 -0400

    Issue #5 - Use the 5.13 C++ code not the 5.14
    This is needed because it includes the code for non Long channel
    spectra...
    
    Also extended the clean target for sip to clean up the files created by
    sip.

--- main/PyQtGUI/src/dataRetriever.cpp
+++ main/PyQtGUI/src/dataRetriever.cpp
@@ -19,7 +19,7 @@
  *  @brief: API to set and retrieve shared memory information
  */
 
-
+#include <bits/stdc++.h>
 #include <cstring>
 #include <algorithm>
 #include <iostream>
@@ -27,6 +27,10 @@
 #include <memory>
 #include <stddef.h>  // defines NULL
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 #include "dataRetriever.h"
 
@@ -72,8 +76,123 @@ dataRetriever::SetHostPortTest(std::string host, std::string port)
   _port = port;
 }
 
+void
+dataRetriever::InitShMemTest()
+{
+  if (dbg) {
+    std::cout << "Inside dataRetriever::InitShMem()" << std::endl;
+    std::cout << _hostname << "," << _port << std::endl;  
+  }
+  
+  std::array<char, 512> buffer;
+  std::string filename, tmp;
+
+  std::string command = "python3 ";
+  std::string pwd(INSTALLED_IN);
+  filename = std::string(pwd)+"/Script/shm_parser.py";
+  if(getenv("INSTDIR"))
+    command += filename+" "+_hostname+" "+_port;
+  else
+    command += filename;  
+  if (dbg) {
+    std::cout << "command --> " << command << std::endl;
+  }
+  FILE* pipe = popen(command.c_str(), "r");
+
+  if (!pipe)
+    std::cerr << "Couldn't start command." << std::endl;
+
+  while (fgets(buffer.data(), 512, pipe) != NULL) {
+    tmp += buffer.data();
+  }
+  auto returnCode = pclose(pipe);
+
+  tmp.erase(std::remove(tmp.begin(), tmp.end(), '\n'), tmp.end());
+
+  std::istringstream ss(tmp);
+  do {
+    std::string word;
+    ss >> word;
+    shm.push_back(word);
+  } while (ss); 
+  
+  key = shm.at(0);
+  char* name = new char[key.length()+1];
+  std::strcpy(name, key.c_str());
+  if(name == NULL) {
+    perror("Could not translate shared memory name");
+    exit(errno);
+  }
+  size = stoi(shm.at(1));
+  if(size == 0) {
+    fprintf(stderr, "Shared memory size string illegal");
+    exit(-1);
+  }
+  
+  // mapping to memory
+  memsize = sizeof(spec_shared) - SPECBYTES + size;
+
+  shmem = dataRetriever::mapmem(name, memsize);
+  spectra = shmem;
+  if(spectra == (shared_memory *)NULL) {
+    perror("Map to shared memory failed!!");
+    exit(errno);
+  }
+  
+}
+
+spec_shared*
+dataRetriever::mapmem(char* name, unsigned int size)
+#ifdef HAVE_SHM_OPEN
+{
+  int fd = shm_open(name, O_RDWR, S_IRUSR | S_IWUSR);
+  if(fd < 0) {
+    perror("shm_open failed!");
+    return NULL;
+  }
+
+  void* pMem;
+#ifdef HAVE_MMAP
+  pMem = mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+#else
+  read(fd, pMem, size);
+#endif
+
+  if(pMem == (char*)-1) {
+    perror("mmap failed!");
+    return NULL;
+  }
+  close(fd);
+  shm_unlink(name);
+  return (spec_shared*)pMem;
+}
+#else
+{
+  key_t key; // Shared memory key.
+  int   id; // Shared memory size. 
+  char *memory;
+
+  memcpy(&key, name, sizeof(key));
 
 
+  id  = shmget(key, size, 0); // Get the memory key
+  if(id < 0) {
+    return (spec_shared *)NULL;
+  }
+
+  memory = (char *)shmat(id, NULL, SHM_RDONLY);
+
+  return (spec_shared *)memory;
+}
+#endif
+
+char*
+dataRetriever::MemoryTop()
+{
+  char *bottom = (char *)shmem;
+  bottom      += (memsize-1);
+  return      bottom;
+}
 
 void
 dataRetriever::PrintOffsets()
